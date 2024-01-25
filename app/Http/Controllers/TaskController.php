5a515b82e9a1ca1b6a3796458786188cb29c9f66
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-01-25 16:35:06 -0500

    finished with basic HTTP methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
@@ -28,11 +30,9 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        $validated = $request->validate([
-            'title' => 'required|max:255'
-        ]);
+        $validated = $request->validated();
         
         $task = Task::create($validated);
 
@@ -58,17 +58,23 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $validated = $request->validated();
+
+        $task->update($validated);
+
+        return new TaskResource($task);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 
     // https://youtu.be/9FJeoq5z1_Y?si=RGClOdr8dRJpD3r6 19:00
