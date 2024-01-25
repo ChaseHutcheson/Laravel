db39ea53003210252d8f6a0e61f1b91f1185a03c
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-01-25 14:08:00 -0500

    more routes

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,14 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(Request $request) {
-        return response()->json(Task::all());
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new TaskCollection(Task::all());
     }
 
-    // https://youtu.be/9FJeoq5z1_Y?si=EVRhtXl_OUr5CeDY 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255'
+        ]);
+        
+        $task = Task::create($validated);
+
+        return new TaskResource($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    // https://youtu.be/9FJeoq5z1_Y?si=RGClOdr8dRJpD3r6 19:00
 }
